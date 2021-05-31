8b5f73be6847cb3fb9f6fb9e950d75674f7dc1d1
Antonis Kalou <kalouantonis@protonmail.com>
2021-05-31 11:56:16 +0200

    Add intent filtering for ProcessLive

--- lib/web/pages/process/process_live.ex
+++ lib/web/pages/process/process_live.ex
@@ -1,4 +1,4 @@
-defmodule Bonfire.UI.Coordination.ProcessLive do
+efmodule Bonfire.UI.Coordination.ProcessLive do
   use Bonfire.Web, :live_view
   # use Surface.LiveView
   use AbsintheClient, schema: Bonfire.GraphQL.Schema, action: [mode: :internal]
@@ -35,6 +35,53 @@ defmodule Bonfire.UI.Coordination.ProcessLive do
     )}
   end
 
+  @quantity_fields """
+  {
+    has_numerical_value
+    has_unit {
+      label
+      symbol
+    }
+  }
+  """
+
+  @resource_fields """
+  {
+    id
+    name
+    note
+    image
+    onhand_quantity #{@quantity_fields}
+    accounting_quantity #{@quantity_fields}
+  }
+  """
+
+  @intent_fields """
+  {
+    id
+    name
+    note
+    due
+    finished
+    context: in_scope_of
+    available_quantity #{@quantity_fields}
+    resource_quantity #{@quantity_fields}
+    effort_quantity #{@quantity_fields}
+    provider {
+      id
+      name
+      display_username
+      image
+    }
+    receiver {
+      id
+      name
+      display_username
+      image
+    }
+    resource_inventoried_as #{@resource_fields}
+  }
+  """
 
   @graphql """
     query($id: ID) {
@@ -50,38 +97,33 @@ defmodule Bonfire.UI.Coordination.ProcessLive do
           name
           image
         }
-        intended_inputs {
-          id
-          name
-          note
-          due
-          finished
-          context: in_scope_of
-          provider {
-            id
-            name
-            display_username
-            image
-          }
-        }
-        intended_outputs {
-          id
-          name
-          note
-          due
-          finished
-          context: in_scope_of
-          provider {
-            id
-            name
-            display_username
-            image
-          }
-        }
+        intended_inputs #{@intent_fields}
+        intended_outputs #{@intent_fields}
       }
     }
   """
   def process(params \\ %{}, socket), do: liveql(socket, :process, params)
+
+  @graphql """
+    query($id: ID, $filters: agentIntentSearchParams) {
+      process(id: $id) {
+        id
+        name
+        note
+        has_end
+        finished
+        working_agents {
+          id
+          name
+          image
+        }
+        intended_inputs(filters: $filters) #{@intent_fields}
+        intended_outputs(filters: $filters) #{@intent_fields}
+      }
+    }
+  """
+
+  def process_filtered(params \\ %{}, socket), do: liveql(socket, :process, params)
 
 
   defdelegate handle_params(params, attrs, socket), to: Bonfire.Web.LiveHandler
