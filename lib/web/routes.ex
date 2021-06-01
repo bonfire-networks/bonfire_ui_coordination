1703c0a8518f0e77d03b70975e97b0366e778256
Mayel <git@mayel.space>
2021-06-01 22:59:28 +0200

    links

--- lib/web/routes.ex
+++ lib/web/routes.ex
@@ -6,9 +6,9 @@ defmodule Bonfire.UI.Coordination.Routes do
       # pages anyone can view
       scope "/", Bonfire.UI.Coordination do
         pipe_through :browser
-        live "/list/:id", ProcessLive
+        live "/list/:id", ProcessLive, as: ValueFlows.Process
         live "/lists", ProcessesLive
-        live "/task/:id", TaskLive
+        live "/task/:id", TaskLive, as: ValueFlows.Planning.Intent
         live "/my-tasks", MyTasksLive
       end
 
