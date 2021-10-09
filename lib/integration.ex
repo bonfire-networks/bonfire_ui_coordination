bce61fbc52aa446a3ea8daec0dea4b7d5a5c35c8
Mayel <git@mayel.space>
2021-10-09 17:37:15 +1300

    task type for intents

--- lib/integration.ex
+++ lib/integration.ex
@@ -4,4 +4,6 @@ defmodule Bonfire.UI.Coordination.Integration do
 
   def mailer, do: Bonfire.Common.Config.get!(:mailer_module)
 
+  def remote_tag_id, do: "https://bonjour.bonfire.cafe/pub/actors/Task" # TODO: put in config
+
 end
