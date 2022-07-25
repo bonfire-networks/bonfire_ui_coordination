a65b19b958b0e523ebed55317bebbbf9c7b8ac13
Mayel de Borniol <git@mayel.space>
2022-07-25 15:56:10 +1200

    https://github.com/bonfire-networks/bonfire-app/issues/426

--- lib/web/components/create_task/create_task_live.ex
+++ lib/web/components/create_task/create_task_live.ex
@@ -4,5 +4,6 @@ defmodule Bonfire.UI.Coordination.CreateTaskLive do
   prop intent_url, :string, required: false, default: ""
   prop action, :string, required: false
   prop output_of_id, :string, required: false
+  prop textarea_class, :css_class, required: false
 
 end
