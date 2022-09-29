a02f16da318578aff1af588ac435956b4b41b4c5
Mayel de Borniol <git@mayel.space>
2022-09-29 20:34:56 +1300

    props

--- lib/web/components/widgets/widget_task_actions/widget_task_actions_live.ex
+++ lib/web/components/widgets/widget_task_actions/widget_task_actions_live.ex
@@ -1,6 +1,6 @@
 defmodule Bonfire.UI.Coordination.WidgetTaskActionsLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
-  prop intent, :any
-  prop widget_title, :string
+  prop intent, :any, default: nil
+  prop widget_title, :string, default: nil
 end
