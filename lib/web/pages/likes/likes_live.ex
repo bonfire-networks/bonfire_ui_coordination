f3b3c6aee9a53bf0f959d06c46d3b49533ec95b8
ivanminutillo <ivanminutillo@outlook.it>
2022-11-02 10:44:26 +0100

    without tabs

--- lib/web/pages/likes/likes_live.ex
+++ lib/web/pages/likes/likes_live.ex
@@ -27,6 +27,7 @@ defmodule Bonfire.UI.Coordination.LikesLive do
        loading: false,
        selected_tab: "favourited",
        page: "likes",
+       hide_tabs: true,
        page_title: l("Important tasks"),
        create_object_type: :task,
        smart_input_prompt: l("Add a task"),
