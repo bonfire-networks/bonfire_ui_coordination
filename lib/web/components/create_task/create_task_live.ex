97a3fa6a2edf7a6dae557a213b541435267933c7
Mayel de Borniol <git@mayel.space>
2023-02-07 12:02:13 +1300

    smart_input_opts

--- lib/web/components/create_task/create_task_live.ex
+++ lib/web/components/create_task/create_task_live.ex
@@ -3,7 +3,7 @@ defmodule Bonfire.UI.Coordination.CreateTaskLive do
 
   prop context_id, :string, default: nil
 
-  prop smart_input_opts, :any, default: nil
+  prop smart_input_opts, :list, default: []
   prop textarea_class, :css_class, required: false
   # unused but workaround surface "invalid value for property" issue
   prop textarea_container_class, :css_class
