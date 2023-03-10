949a75796703d22924eca8d06efd1dbb2f13d87a
Mayel de Borniol <git@mayel.space>
2023-03-10 17:25:08 +1300

    p

--- lib/web/components/task_hero/task_hero_live.ex
+++ lib/web/components/task_hero/task_hero_live.ex
@@ -2,6 +2,6 @@ defmodule Bonfire.UI.Coordination.TaskHeroLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
   prop task, :map
-  prop showing_within, :any, default: :task
+  prop showing_within, :atom, default: :task
   prop is_editable?, :boolean, default: false
 end
