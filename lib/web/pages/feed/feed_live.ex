002fff42a345fa5b08d0f576488ff167b6e9e1d6
Mayel de Borniol <git@mayel.space>
2023-11-18 15:26:40 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/722

--- lib/web/pages/feed/feed_live.ex
+++ lib/web/pages/feed/feed_live.ex
@@ -27,7 +27,6 @@ defmodule Bonfire.UI.Coordination.FeedLive do
        page_info: nil,
        loading: false,
        feed_title: l("My coordination feed"),
-       feed_component_id: :feeds,
        feed_id: nil,
        feed_ids: nil,
        object_types: object_types,
