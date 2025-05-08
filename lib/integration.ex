d1693c38bf226c07b61eafec423e707d297e2b3b
Mayel de Borniol <git@mayel.space>
2025-05-08 15:29:38 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1238

--- lib/integration.ex
+++ lib/integration.ex
@@ -8,6 +8,6 @@ defmodule Bonfire.UI.Coordination do
 
   def remote_tag_prefix, do: nil
 
-  # def remote_tag_prefix, do: "https://bonjour.bonfire.cafe/pub/actors/" # TODO: put in config
+  # def remote_tag_prefix, do: "https://bonjour.bonfire.cafe/pub/actors/" 
   def remote_tag_id, do: "#{remote_tag_prefix()}Task"
 end
